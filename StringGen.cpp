fc50923cd58f3e3b371eb51e94ff002e4cf1af4b
Nasir Ali <nasiralis1731@gmail.com>
2020-03-30 13:37:40 +0500

    Create StringGen.cpp

---
+++ StringGen.cpp
@@ -0,0 +1,20 @@
+/* CODED BY NASIR ALI */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+cout << " ALPHABETS : ";
+for(int x=0x41;x<=0x5A;x++){cout << (char)x;}
+cout << endl;
+
+cout << " NUMBERS : ";
+for(int x=0x30;x<=0x39;x++){cout << (char)x;}
+cout << endl;
+    
+cout << " SYMBOLS : ";
+for(int x=0x21;x<=0x2F;x++){cout << (char)x;}
+cout << endl;
+    
+}
